f2a0d4948a012504ed51021bf8275adb71fa0f29
tasija03 <anastasija.divjak03@gmail.com>
2026-04-27 14:44:48 +0200

    Dodavanje dragon.obj fajla i pravljanje shadera za isti

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,8 +1,37 @@
 #include <engine/core/Engine.hpp>
+#include <spdlog/spdlog.h>
 
-/**
- * Start here...
- */
+class DragonController : public engine::core::Controller {
+private:
+    engine::resources::Model* my_dragon = nullptr;
+    engine::resources::Shader* m_shader = nullptr;
+
+protected:
+    void initialize() override {
+        auto res = engine::core::Controller::get<engine::resources::ResourcesController>();
+
+        my_dragon = res->model("dragon");
+        m_shader = res->shader("shader");
+    }
+
+    void draw() override {
+        if (!my_dragon || !m_shader) {
+            spdlog::error("NULL dragon or shader!");
+            return;
+        }
+
+        my_dragon->draw(m_shader);
+    }
+};
+
+class MyApp : public engine::core::App {
+protected:
+    void app_setup() override {
+        register_controller<DragonController>();
+    }
+};
+
 int main(int argc, char **argv) {
-    return 0;
+    MyApp app;
+    return app.run(argc, argv);
 }
