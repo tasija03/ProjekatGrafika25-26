41f67dcd3887d44f7b557b283d366a46c2c949a0
tasija03 <anastasija.divjak03@gmail.com>
2026-08-03 17:24:55 +0200

    making controller for wheel

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,33 +1,58 @@
+#include "engine/graphics/GraphicsController.hpp"
+
+
 #include <engine/core/Engine.hpp>
 #include <spdlog/spdlog.h>
 
-class DragonController : public engine::core::Controller {
+class WheelController : public engine::core::Controller {
 private:
-    engine::resources::Model* my_dragon = nullptr;
+    engine::resources::Model* m_wheel = nullptr;
     engine::resources::Shader* m_shader = nullptr;
 
 protected:
     void initialize() override {
+        engine::graphics::OpenGL::enable_depth_testing();
+
         auto res = engine::core::Controller::get<engine::resources::ResourcesController>();
 
-        my_dragon = res->model("dragon");
+        m_wheel = res->model("wheel");
         m_shader = res->shader("shader");
+
     }
 
     void draw() override {
-        if (!my_dragon || !m_shader) {
+        if (!m_wheel|| !m_shader) {
             spdlog::error("NULL dragon or shader!");
             return;
         }
 
-        my_dragon->draw(m_shader);
+        auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        camera->camera()->Position = glm::vec3(0.0f, 0.0f, 3.0f);
+
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+
+        m_shader->use();
+        
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.01f));
+
+        m_shader->set_mat4("model", model);
+        m_shader->set_mat4("view", graphics->camera()->view_matrix());
+        m_shader->set_mat4("projection", graphics->projection_matrix());
+
+        m_wheel->draw(m_shader);
+
     }
+
 };
 
 class MyApp : public engine::core::App {
 protected:
     void app_setup() override {
-        register_controller<DragonController>();
+        auto wheel_controller = register_controller<WheelController>();
+        wheel_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
     }
 };
 
