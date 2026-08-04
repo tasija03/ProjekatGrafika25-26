4024629a27c407f3f1d6a4ad6414345bf59ce818
tasija03 <anastasija.divjak03@gmail.com>
2026-08-04 12:40:06 +0200

    dodat izlazak na esc dugme

---
+++ app/include/MainController.hpp
@@ -0,0 +1,13 @@
+#ifndef MYCONTROLLER_HPP
+#define MYCONTROLLER_HPP
+
+#include <engine/core/Controller.hpp>
+
+namespace app {
+    class MainController : public engine::core::Controller{
+        void initialize() override;
+        bool loop() override;
+    };
+}
+
+#endif
